f424841b406515cdbbfe585f030dc9cb9f4aeaa1
Dallin Savage <60589051+killer272@users.noreply.github.com>
2020-06-01 16:38:28 -0600

    Add files via upload

---
+++ Chapter 1/Exercise_1_9.java
@@ -0,0 +1,13 @@
+/*Dallin Savage
+ * 6/1/20
+ * Calculation
+ */
+public class Exercise_1_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Area:" + (4.5 * 7.9));
+		System.out.println("Perimeter:" + (4.5 + 4.5 + 7.9 + 7.9));
+	}
+	
+}
