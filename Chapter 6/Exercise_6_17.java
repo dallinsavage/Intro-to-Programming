765af0ff586f3ba6bfe0d472eba38486794fc984
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-08 18:54:47 -0600

    Add files via upload

---
+++ Chapter 6/Exercise_6_17.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercise_6_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter n");
+		int n = input.nextInt();
+		int counter = 0;
+		int counterl = 0;	
+		while (counterl < n) {
+			int random = (int) (Math.random() * 10);
+			if (random >= 5) {
+				random = 1;
+			}
+			else {
+				random = 0;
+			}
+				while (counterl < n) {
+					System.out.printf("%d", random);
+					counter++;
+					if (counter == n) {
+						System.out.println("/n");
+						counterl++;
+			}
+		}
+	}
+}
+}
