1c15d14a865c8bad728d5b7fb0fb72aa560f8927
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-09 18:00:37 -0600

    Add files via upload

---
+++ Chapter 6/Exercise_6_37.java
@@ -0,0 +1,15 @@
+
+public class Exercise_6_37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	public static String format(int number, int width) {
+		while (width > 0) {
+			String front = "0";
+			front = front + "0";
+			width--;
+		}
+	}
+}
