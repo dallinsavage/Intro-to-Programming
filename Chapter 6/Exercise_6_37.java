25d691cf10c0b93067b49b69b566271b5eeef88e
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-10 15:46:23 -0600

    Update Exercise_6_37.java

--- Chapter 6/Exercise_6_37.java
+++ Chapter 6/Exercise_6_37.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
 
 public class Exercise_6_37 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter number");
+		int num = input.nextInt();
+		System.out.println("enter width");
+		int width = input.nextInt();
+		String finish = format(num, width);
+		System.out.println(finish);
 	}
 	
 	public static String format(int number, int width) {
-		while (width > 0) {
-			String front = "0";
+		int counter = 0;
+		String front = "";
+		String end = Integer.toString(number);
+		while (counter < width) {
 			front = front + "0";
-			width--;
+			counter++;
 		}
+		String finish = ( front + end);
+		return finish;
 	}
 }
