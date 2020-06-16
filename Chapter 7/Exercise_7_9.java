addb0946de46cbf90d49eb37b59b3fd68f322b70
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-16 16:16:24 -0600

    Update Exercise_7_9.java

--- Chapter 7/Exercise_7_9.java
+++ Chapter 7/Exercise_7_9.java
@@ -3,6 +3,7 @@
  * find the min of input
  */
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Exercise_7_9 {
 
@@ -11,10 +12,17 @@ public class Exercise_7_9 {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter 10 numbers seperated by a space");
 		double[] list = new double[10];
-		int x;
-		for (x = 0; x < (list.length - 1); x++); {
+		int x = 0;
+		for (x = 0; x < (list.length); x++) {
 			list[x] = input.nextDouble();
 		}
+		double minimum = min(list);
+		System.out.println("The minimum number is " + minimum);	
+	}
+		
+public static double min(double[] array) {
+	java.util.Arrays.parallelSort(array);
+	return array[0];
 	}
 
 }
