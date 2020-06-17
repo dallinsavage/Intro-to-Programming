57995e88b72cef4d0df18dba152f78ee06022183
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-17 17:57:29 -0600

    Update Exercise_7_15.java

--- Chapter 7/Exercise_7_15.java
+++ Chapter 7/Exercise_7_15.java
@@ -11,31 +11,55 @@ public class Exercise_7_15 {
 		for (x = 0; x < (list.length); x++) {
 			list[x] = input.nextInt();
 		}
-		System.out.println(list);
 		int[] finallist = eliminateDuplicates(list);
-		System.out.println("Array without duplicates " + finallist);
+		System.out.println("Array without duplicates ");
+		for (int z = 0;  z < finallist.length; z++) {
+			System.out.printf("%d ", finallist[z]);
+		}
 	}
 	
 	public static int[] eliminateDuplicates(int[] array) {
-		int x = 1;
-		int i = 0;
-		int y = 0;
 		int[] newList = new int[10];
-		System.out.println(newList);
-		for (i = 0; i < array.length - 1; i++) {
-			if (i == x) {
-			}
-			else if (array[i] == array[x]) {
-				newList[x] = -1;
-				x++;
-			}
-			
-			if (array[i] != -1) {
+		for (int i = 0; i < array.length - 1; i++) {
+			int z = 1;
+			int y = 0;
+			if (array[i] != array[z]) {
 				newList[y] = array[i];
+				z++;
 				y++;
-				
+			}
+			else {
+				z++;
 			}
 		}
+/*			int x = 1;
+			int counter = 1;
+			if (array[i] == array[x]) {
+				counter++;
+			}
+			for (int y = 0; y < array.length - 1; y++) {
+				if (counter == 1) {
+				newList[y] = array[i];
+			}
+			}
+		}
+		*/
+		/*		for (int i = 0; i < array.length - 2; i++) {
+			int x = 1;
+			for (x = 1; x < array.length - 1; x++) {
+			if (array[i] == array[i + x]) {
+				array[i] = 0;
+			}
+			}
+		}
+		for (int y = 0; y < array.length - 1; y++) {
+			int z = 0;
+			if (array[y] != 0) {
+				newList[z] = array[y];
+				y++;
+			}
+}
+*/
 		return newList;
 	}
 
