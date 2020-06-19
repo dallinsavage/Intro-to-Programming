420f2a410bf8b4f30a32fb18a54a19be2b2c6919
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-19 16:06:36 -0600

    Update Exercise_7_23.java

--- Chapter 7/Exercise_7_23.java
+++ Chapter 7/Exercise_7_23.java
@@ -7,20 +7,24 @@ public class Exercise_7_23 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean[] lockers = new boolean[100];
-		for (int i = 0; i < lockers.length - 1; i++) {
-			for (int x = 0; x < lockers.length - 1; x += x) {
-			if (lockers[i + x] == true) {
-				lockers[i + x] = false;
-				x =+ x;
+		for (int z = 0; z < lockers.length; z++) {
+			lockers[z] = true;
+		}
+		for (int i = 1; i < lockers.length; i++) {
+			for (int x = i; x < lockers.length; x++) {
+				if (lockers[0 + x] == true) {
+				lockers[0 + x] = false;
+				x += i;
 			}
 			else {
-				lockers[i + x] = true;
+				lockers[0 + x] = true;
+				x += i;
 			}
 		}
 	}
 		System.out.println(" lockers that are open:");
-		int counter = 0;
-		for (int y = 0; y < lockers.length - 1; y++) {
+		int counter = 1;
+		for (int y = 0; y < lockers.length; y++) {
 			if (lockers[y] == true) {
 				System.out.println(counter);
 				counter++;
@@ -30,5 +34,4 @@ public class Exercise_7_23 {
 			}
 		}
 	}
-
 }
