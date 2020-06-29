8483cde29ee3a62dce416e4ae7a31a406709167d
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-06-29 15:34:31 -0600

    Update Exercise_7_15.java

--- Chapter 7/Exercise_7_15.java
+++ Chapter 7/Exercise_7_15.java
@@ -1,54 +1,90 @@
-/* Dallin Savage
- * 6/18/20
- * eliminate duplicate numbers from an array
+/*Dallin Savage
+ * 6/25/20
+ * Basic budget program
  */
 import java.util.Scanner;
 
-public class Exercise_7_15 {
+public class Main_Budget {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String budget[] = new String[10];
+		double amount[] = new double[10];
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter 10 numbers seperated by a space");
-		int[] list = new int[10];
-		int x = 0;
-		for (x = 0; x < (list.length); x++) {
-			list[x] = input.nextInt();
+		double income = 0;
+		for (int j = 0; j < budget.length; j++) {
+			budget[j] = "";
 		}
-		int[] finallist = eliminateDuplicates(list);
-		System.out.println("Array without duplicates ");
-		for (int z = 0;  z < finallist.length; z++) {
-			if (finallist[z] != 0) {
-				System.out.printf("%d ", finallist[z]);
-			}
-			
+		
+		//get categories and amounts
+		System.out.println("Begin you monthly budget");
+		System.out.println("How may budget categories do you want to enter? (up to 10)");
+		int z = input.nextInt();
+		for (int i = 0; i < z; i++) {
+			System.out.println("Enter a budget category:");
+			budget[i] = input.next();
+			System.out.println("Enter budget amount: $");
+			amount[i] = input.nextDouble();
 		}
-	}
-	
-	public static int[] eliminateDuplicates(int[] array) {
-		int[] newList = new int[10];
-		for (int i = 0; i < array.length - 1; i++) {
-			int counter = 1;
-			int y = i + 1;
-			if (array[i] == array[y]) {
-				counter++;
-				y++;
-			}
-			else {
-				y++;
-			}
-		if (counter == 1 && y != array.length) {
-				newList[i] = array[i];
+		System.out.println("Enter Actual income: $");
+		income = input.nextDouble();
+		double mtotal = 0;
+		
+		//get total budget amount
+		for (int x = 0; x < amount.length; x++) {
+			mtotal = mtotal + amount[x];
+		}
+		
+		// enter more income
+		System.out.println("Enter another month of income?");
+		System.out.println("yes or no");
+		String answer = input.next();
+		answer = answer.toLowerCase();
+		int counter = 1;
+		while (answer.equals("yes")) {
+			System.out.println("Enter month " + (counter + 1) + " income: $");
+			double nextIncome = input.nextDouble();
+			income = income + nextIncome;
+			System.out.println("Enter another month of income?");
+			System.out.println("yes or no");
+			answer = input.next();
+			answer = answer.toLowerCase();
+			counter++;
+		}
+		double endAmount = income - mtotal * counter;
+		
+		//print budgets and amounts
+
+		printBudget(budget, amount, z);
+		System.out.printf("One month budget $%4.2f\n", mtotal);
+		if (counter > 1) {
+		System.out.printf(counter + " month budget $%4.2f\n", mtotal * counter);
+		}
+		if (counter == 1) {
+		System.out.printf(counter + " month income : $%4.2f\n", income);
 		}
 		else {
-				newList[i] = 0;
+			System.out.printf(counter + " months income : $%4.2f\n", income);	
 		}
-		if (counter != 1 && y == array.length) {
-			newList[9] = array[9];
+		if (endAmount >= 0) {
+			System.out.printf("Extra : $%4.2f\n", endAmount);
 		}
-					
-		}		
-		return newList;
+		else {
+			System.out.println("You are over budget!");
+			System.out.printf("Total Expense $%4.2f", endAmount);
+		}
 	}
-
+	public static void printBudget(String[] budget, double[] amount, int z) {
+		System.out.println("------------------");
+		for (int y = 0; y < z; y++) {
+			if (budget[y].equals("") != true) {
+			System.out.printf("| %s: $", budget[y]);
+			}
+			if (amount[y] != 0) {
+			System.out.printf("%4.2f |\n", amount[y]);
+			}
+		System.out.println("------------------");
+		
+	}
+	}
 }
